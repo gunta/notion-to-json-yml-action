ad833349f1063ba65cef01a3335a05629b5b3c50
Gunther Brunner <gunta85@gmail.com>
2022-11-11 02:36:40 +0900

    Update

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello ${nameToGreet}!`)
+    const notionToken = core.getInput('notion-token')
+    const notionDb = core.getInput('notion-db')
+    const outputFile = core.getInput('output-file')
+
+
+    console.log(`Hello ${outputFile}!`)
     const time = (new Date()).toTimeString()
     core.setOutput("time", time)
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`Event payload: ${payload}`)
 } catch (error) {
     core.setFailed(error.message)
 }
